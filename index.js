2b8851fb7bd7ef38ebcae2ce5936aa15194e127f
Georgy Chirkov <mail@gich.co>
2014-06-27 18:03:22 +0400

    Serve static content

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ module.exports = exports = (function (rq) {
     var cfg = rq(__dirname + '/modules/config')
         , Key = rq(__dirname + '/modules/Key')
         , http = rq('http')
-        , fs = rq('fs');
+        , fs = rq('fs')
+        , st = rq('node-static')
+        , fileServer = new st.Server(__dirname + cfg["static_dir"], { cache: 3600 });
+
     var dir = __dirname + "/" + cfg["directory_name"];
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
@@ -29,90 +32,95 @@ module.exports = exports = (function (rq) {
     }
 
     http.createServer(function (req, res) {
-        var key = req.url.slice(1)
-            , curDate = new Date().getTime();
-        if (!key || key in cfg["excluded_keys"]) {
-            respond(200, "sup :)", res);
-            return;
-        }
-        Key.Exists(key, function (exists) {
-            if (exists) {
-                Key.Get(key, function (err, data) {
-                    if (err) {
-                        console.log(err);
-                        return respond(200, err.toString(), res);
-                    } else {
-                        if (data.anonymize) {
-                            respond(200, cfg["redirect-template"].replace("_%_", data.url), res, {
-                                'Content-Type': 'text/html; charset=utf-8'
-                            });
-                        } else {
-                            respond(302, null, res, {
-                                'Location': data.url
-                            });
+        if (req.url.indexOf('/api') === 0) {
+            var url = req.url.substr(1)
+                , slash = url.indexOf('/')
+                , method = url.substr(0, slash)
+                , postData = "";
+            if (req.method == 'OPTIONS') {
+                return respond(200, null, res);
+            }
+            if (req.method != 'POST') {
+                return respond(405, {error: "Method not supported"}, res);
+            }
+            req.on('data', function (chunk) {
+                postData += chunk.toString();
+            });
+            req.on('end', function () {
+                switch (method) {
+                    case "create":
+                        try {
+                            postData = JSON.parse(postData);
+                        } catch (e) {
+                            console.log(e);
+                            return respond(200, {error: e.toString()}, res);
                         }
-                        if (data['expiry'] && data['expiry'] <= curDate) {
-                            Key.Remove(key, function (err) {
+                        url = postData.url;
+                        var key = Key.random();
+                        Key.Create({
+                            url: url,
+                            expiry: postData.expiry,
+                            anonymize: postData.anonymize
+                        }, function (err, key) {
+                            return respond(200, {
+                                error: err,
+                                url: key ? ("http://" + cfg["hostname"] + "/" + key) : undefined,
+                                originalUrl: url
+                            }, res);
+                        });
+                        break;
+                    default:
+                        return respond(200, {error: "wrong method"}, res);
+                        break;
+                }
+            });
+        } else {
+            req.addListener('end', function () {
+                if (req.url == '/') {
+                    return fileServer.serveFile('/index.html', 200, {}, req, res);
+                }
+                fs.exists(__dirname + cfg["static_dir"] + req.url, function (exists) {
+                    if (exists) {
+                        return fileServer.serve(req, res);
+                    }
+                    var key = req.url.slice(1)
+                        , curDate = new Date().getTime();
+                    Key.Exists(key, function (exists) {
+                        if (exists) {
+                            Key.Get(key, function (err, data) {
                                 if (err) {
                                     console.log(err);
+                                    return respond(200, err.toString(), res);
                                 } else {
-                                    console.log("deleted key", key)
+                                    if (data.anonymize) {
+                                        respond(200, cfg["redirect-template"].replace("_%_", data.url), res, {
+                                            'Content-Type': 'text/html; charset=utf-8'
+                                        });
+                                    } else {
+                                        respond(302, null, res, {
+                                            'Location': data.url
+                                        });
+                                    }
+                                    if (data['expiry'] && data['expiry'] <= curDate) {
+                                        Key.Remove(key, function (err) {
+                                            if (err) {
+                                                console.log(err);
+                                            } else {
+                                                console.log("deleted key", key)
+                                            }
+                                        })
+                                    }
                                 }
                             })
+                        } else {
+                            respond(302, null, res, {'Location': "http://" + cfg["hostname"] + "/"});
                         }
-                    }
-                })
-            } else {
-                respond(302, null, res, {'Location': "http://" + cfg["hostname"] + "/"});
-            }
-        });
+                    });
+                });
+            }).resume();
+        }
     }).listen(cfg["port"]);
     console.log("server listens on port", cfg["port"]);
-
-    http.createServer(function (req, res) {
-        var url = req.url.substr(1)
-            , slash = url.indexOf('/')
-            , method = url.substr(0, slash)
-            , postData = "";
-        if (req.method == 'OPTIONS') {
-            return respond(200, null, res);
-        }
-        if (req.method != 'POST') {
-            return respond(405, {error: "Method not supported"}, res);
-        }
-        req.on('data', function (chunk) {
-            postData += chunk.toString();
-        });
-        req.on('end', function () {
-            switch (method) {
-                case "create":
-                    try {
-                        postData = JSON.parse(postData);
-                    } catch (e) {
-                        console.log(e);
-                        return respond(200, {error: e.toString()}, res);
-                    }
-                    url = postData.url;
-                    var key = Key.random();
-                    Key.Create({
-                        url: url,
-                        expiry: postData.expiry,
-                        anonymize: postData.anonymize
-                    }, function (err, key) {
-                        return respond(200, {
-                            error: err,
-                            url: key ? ("http://" + cfg["hostname"] + "/" + key) : undefined,
-                            originalUrl: url
-                        }, res);
-                    });
-                    break;
-                default:
-                    return respond(200, {error: "wrong method"}, res);
-                    break;
-            }
-        });
-    }).listen(cfg["api_port"]);
-    console.log("api server listens on port", cfg["api_port"]);
 
     return Key;
 })(require);
